eb00cf45426ec06354bb3d83135bae4671917ce0
Logan Watanabe <lwatanab@andrew.cmu.edu>
2013-05-11 10:43:22 -0400

    initial, nothing generated yet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SummerFun::Application.config.secret_token = 'f249fbaa3f05455d0ddf6e76e171a07da21581f97e445fe57d45f6c8cbe9e0744cbd4464a05bccf9582a5a7c32364d4757aa9480f5e2517d9b3760bd82306b5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SummerFun::Application.config.session_store :cookie_store, key: '_SummerFun_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SummerFun::Application.config.session_store :active_record_store
